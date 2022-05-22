acee1da58dc46426214bbd240b1d495fad171ecf
epthalite <fmy628.1998@icloud.com>
2022-05-22 23:56:12 +0900

    change jest

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -1,38 +1,48 @@
 import React, { useState } from "react";
 
+const getUser = async () => {
+  return { id: "1", name: "Robin" };
+};
+
 const Sample = () => {
-  const [search,setSearch] = useState('');
-    function handleChange(event) {
+  const [search, setSearch] = useState("");
+  const [user, setUser] = React.useState(null);
+
+  function handleChange(event) {
     setSearch(event.target.value);
   }
+
+  React.useEffect(() => {
+    const loadUser = async () => {
+      const user = await getUser();
+      setUser(user);
+    };
+
+    loadUser();
+  }, []);
+
   return (
     <>
-    <div>
-      <Search value={search} onChange={handleChange}>
-        Search:
-      </Search>
-
-      <p>Searches for {search ? search : '...'}</p>
-    </div>
+      <div>
+        {user ? <p>Signed in as {user.name}</p> : null}
+        <Search value={search} onChange={handleChange}>
+          Search:
+        </Search>
+        <p>Searches for {search ? search : "..."}</p>
+      </div>
     </>
   );
 };
 
-const Search = ({value,onChange,children})=>{
-
+const Search = ({ value, onChange, children }) => {
   return (
-  <>
-    <div>
-      <label htmlFor="search">{children}</label>
-      <input
-        id="search"
-        type="text"
-        value={value}
-        onChange={onChange}
-      />
-    </div>
-  </>)
-
-}
+    <>
+      <div>
+        <label htmlFor="search">{children}</label>
+        <input id="search" type="text" value={value} onChange={onChange} />
+      </div>
+    </>
+  );
+};
 
 export default Sample;
--- src/__tests__/Sample.test.js
+++ src/__tests__/Sample.test.js
@@ -1,12 +1,34 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import Sample from "../Sample";
 
 describe("App", () => {
   test("renders App component", () => {
-    const { debug } = render(<Sample />);
+    render(<Sample />);
     // package.json に設定を書いて回避。
-    debug();
+    // screen.debug();
+
+    // screen.debug();
+    // 完全一致
+    expect(screen.getByText("Search:")).toBeInTheDocument();
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
+
+    // fails
+    // expect(screen.getByText(/Searches for JavaScript/)).toBeNull();
+
+    // success 存在しない要素のアサーションを行うためには、getByをqueryByに置換すると良いでしょう。
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+  });
+
+  test("async await", async () => {
+    // 非同期で最後に存在する場合は findBy を使います。
+    render(<Sample />);
+    expect(screen.queryByText(/Signed in as/)).toBeNull();
+
+    screen.debug();
+    expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
+
+    screen.debug();
   });
 });
